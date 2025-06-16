6ea2008ee242cb726810afbc8c1d8825cbf0ef08
idontknowlolwhattoput <132071917+idontknowlolwhattoput@users.noreply.github.com>
2025-06-15 22:11:09 -0700

    today

--- portfolio/src/components/Main.jsx
+++ portfolio/src/components/Main.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 function Main() {
+
+    const icons = [
+        {
+          iconname: "About me",
+          src: './src/img/about.svg',
+          default: "about me"
+        },
+        {
+          iconname: "CV/Skills",
+          src: './src/img/cv.svg',
+          default: "about me"
+        },
+        {
+          iconname: "Contacts",
+          src: './src/img/contacts.svg',
+          default: "contacts"
+        },
+        {
+          iconname: "Email me!",
+          src: './src/img/email.svg',
+          default: "about me"
+        },
+       
+    ]
+
     return (
-          <div className="flex flex-col gap-5 p-5 w-screen h-screen">
-             <div className="bg-red-500 w-20 h-20"></div>
-             <div className="bg-red-500 w-20 h-20"></div>
-            
+          <div className="flex flex-col p-5 gap-3 w-screen h-screen">
+            {icons.map((ico, key) =>(
+                 <div className="flex flex-col gap-1 items-center w-25 h-25" key={key}>
+                    <img src={ico.src} className="w-15 h-15 "/>
+                    <p className="pixel text-center text-white text-lg">{ico.iconname}</p>
+                 </div>
+            ))}
           </div>
     )
 }
